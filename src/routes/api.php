ccab85aef2649060fdf1724d21afd73fd09050fd
Takeru Kurihara <t.kurihara@hybrid-technologies.co.jp>
2024-06-24 18:20:47 +0900

    frist commit

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiProductController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('products', ApiProductController::class);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('products', ApiProductController::class);
+});
+
+Route::post("login",[UserController::class,'index']);
